220b1983c38b95aa202cc481bbdb02d75a6cb97a
Halil İbrahim Direktör <hidirektor@gmail.com>
2025-01-05 07:27:43 +0300

    Module Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,3 @@
 module me.t3sl4.util.file {
-    requires java.net.http;
-    requires org.apache.httpcomponents.httpclient;
-    requires org.apache.httpcomponents.httpcore;
-    requires java.prefs;
-
     exports me.t3sl4.util.file;
 }
